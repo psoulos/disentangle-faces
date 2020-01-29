560134a633978c70fc34a729cf00a1783379946d
Paul Soulos <psoulos@gmail.com>
2020-01-29 13:33:36 -0500

    Remove unused stimuli files from the condition ids

--- stimuli_filename_to_condition_id.py
+++ stimuli_filename_to_condition_id.py
@@ -10,11 +10,20 @@ condition_id_to_filename = {}
 normal_face_filename_prefixes = ['f', 'pf', 'pu', 'u']
 scrambled_face_filename_prefixes = ['ps', 's']
 
+unused_stimuli = ['f076.bmp', 'f117.bmp', 'f147.bmp', 'f148.bmp', 'f149.bmp',
+                  'f150.bmp', 'pf001.bmp', 'pf002.bmp', 'pf003.bmp', 'pf004.bmp',
+                  'ps001.bmp', 'ps002.bmp', 'ps003.bmp', 'ps004.bmp', 'pu001.bmp',
+                  'pu002.bmp', 'pu003.bmp', 'pu004.bmp', 's076.bmp', 's117.bmp',
+                  's147.bmp', 's148.bmp', 's149.bmp', 's150.bmp', 'u076.bmp', 'u117.bmp']
+
 normal_face_ids = []
 scrambled_face_ids = []
 
 index = 1
 for file in files:
+    if file in unused_stimuli:
+        print('skipping {}'.format(file))
+        continue
     filename_to_condition_id[file] = index
     condition_id_to_filename[index] = file
     for prefix in normal_face_filename_prefixes:
