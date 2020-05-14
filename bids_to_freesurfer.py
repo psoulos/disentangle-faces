1b51d0428b2e455285ef38cf70268eaa6f96d7da
Paul Soulos <psoulos@gmail.com>
2020-05-14 15:09:47 -0400

    Create run list files while converting from BIDS to freesurfer

--- bids_to_freesurfer.py
+++ bids_to_freesurfer.py
@@ -1,14 +1,16 @@
 import os
 import argparse
 import shutil
+import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--bids_dir', type=str, help='The location of the BIDS directory')
-parser.add_argument('--output_dir', type=str, help='The location of the output directory')
+parser.add_argument('--freesurfer_dir', type=str, help='The location of the output freesurfer '
+                                                       'directory')
 args = parser.parse_args()
 
-if not os.path.exists(args.output_dir):
-    os.makedirs(args.output_dir)
+if not os.path.exists(args.freesurfer_dir):
+    os.makedirs(args.freesurfer_dir)
 
 for subject_dir in os.listdir(args.bids_dir):
     print('Processing {}'.format(subject_dir))
@@ -17,7 +19,7 @@ for subject_dir in os.listdir(args.bids_dir):
         print('Processing session {}'.format(sess_dir))
         freesurfer_subject_name = 'vaegan-{}-{}'.format(subject_dir, sess_dir)
         freesurfer_subject_dir = os.path.join(
-            args.output_dir, freesurfer_subject_name)
+            args.freesurfer_dir, freesurfer_subject_name)
         os.makedirs(freesurfer_subject_dir)
 
         # Copy the anatomical files
@@ -31,15 +33,42 @@ for subject_dir in os.listdir(args.bids_dir):
         freesurfer_bold_dir = os.path.join(freesurfer_subject_dir, 'bold')
         bids_bold_dir = os.path.join(subject_path, sess_dir, 'func')
         os.makedirs(freesurfer_bold_dir)
-        # Print the original BIDS runs into a file for inspecting later
-        with open(os.path.join(freesurfer_bold_dir, 'orig_run_names.txt'), 'w') as f:
-            for i in sorted(os.listdir(bids_bold_dir)):
-                f.write('{}\n'.format(i))
 
+        localizer_runs = sorted(glob.glob(os.path.join(bids_bold_dir, '*loc*.nii.gz')))
+        functional_runs = sorted(glob.glob(os.path.join(bids_bold_dir, '*faces*.nii.gz')))
+
+        orig_run_names_file = open(os.path.join(freesurfer_bold_dir, 'orig_run_names.txt'), 'w')
+        localizer_run_list_file = open(os.path.join(freesurfer_bold_dir, 'rlf_localizer.txt'), 'w')
+        faces_run_list_file = open(os.path.join(freesurfer_bold_dir, 'rlf_faces.txt'), 'w')
         # Increment this for each run
         run_num = 1
 
+        # Process the localizer runs
+        for localizer_run in localizer_runs:
+            orig_run_names_file.write('{:03d}: {}\n'.format(run_num, localizer_run))
+            localizer_run_list_file.write('{:03d}\n'.format(run_num))
+            freesurfer_localizer_dir = os.path.join(freesurfer_bold_dir, '{:03d}'.format(run_num))
+            os.makedirs(freesurfer_localizer_dir)
+            shutil.copyfile(localizer_run, os.path.join(freesurfer_localizer_dir, 'f.nii.gz'))
+            shutil.copy(localizer_run.replace('.nii.gz', '.json'), freesurfer_localizer_dir)
+            shutil.copy(localizer_run.replace('_bold.nii.gz', '_events.tsv'), freesurfer_localizer_dir)
+            run_num += 1
 
+        # Process the functional runs
+        for functional_run in functional_runs:
+            orig_run_names_file.write('{:03d}: {}\n'.format(run_num, functional_run))
+            faces_run_list_file.write('{:03d}\n'.format(run_num))
+            freesurfer_functional_dir = os.path.join(freesurfer_bold_dir, '{:03d}'.format(run_num))
+            os.makedirs(freesurfer_functional_dir)
+            shutil.copyfile(functional_run, os.path.join(freesurfer_functional_dir, 'f.nii.gz'))
+            shutil.copy(functional_run.replace('.nii.gz', '.json'), freesurfer_functional_dir)
+            shutil.copy(functional_run.replace('_bold.nii.gz', '_events.tsv'),
+                        freesurfer_functional_dir)
+            run_num += 1
+
+        orig_run_names_file.close()
+        faces_run_list_file.close()
+        localizer_run_list_file.close()
 
         # Create the subjectname file
         with open(os.path.join(freesurfer_subject_dir, 'subjectname'), 'w') as f:
