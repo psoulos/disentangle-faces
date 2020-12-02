2f241071dd76d3236f43e0b2556c606d59f4a53f
Paul Soulos <psoulos@gmail.com>
2020-12-02 11:46:16 -0500

    Put threshold loop back in

--- correlate_betas.py
+++ correlate_betas.py
@@ -10,11 +10,12 @@ from disentanglement_lib.data.ground_truth.celeba import process_path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--celeba_dir', type=str, help='', required=True)
+parser.add_argument('--skip_p_value', action='store_false')
 args = parser.parse_args()
 
-THRESHOLDS = [2, 3, 4]
-
-subject_nums = [1, 3, 4]
+THRESHOLDS = [2, 3, 4, 5]
+model_type = 'dvae'
+subject_nums = [1, 2, 3, 4]
 
 MODELS = {
     'dvae': 'factor_vae_latent_24_gamma_24_try_1',
@@ -51,7 +52,6 @@ TEST_STIMULI = {
                           'F14520.jpg', 'M14256.jpg']
 }
 
-model_type = 'dvae'
 print('Model {}'.format(model_type))
 saved_model_dir = os.path.join('output', MODELS[model_type], 'tfhub')
 model = tf.saved_model.load(export_dir=saved_model_dir, tags=[], sess=sess)
@@ -61,83 +61,58 @@ for line in open('stimuli/ImageNames2Celeba.txt', 'r'):
     stimulus, celeba = line.split()
     stimulus_to_celeba[os.path.basename(stimulus)] = celeba
 
-#correlation_vectors = {}
 for subject_num in subject_nums:
     print('Subject {}'.format(subject_num))
     localizer_map = sio.loadmat('localizer-maps/sub-{:02d}_sig.mat'.format(subject_num))['data'][0]
-    threshold = 4
-    above_threshold = localizer_map > threshold
-    n_voxels = np.sum(above_threshold)
-    print('Number above threshold: {}'.format(n_voxels))
-
-    print('Model {}'.format(model_type))
-    betas = sio.loadmat('betas/sub-{:02d}_mean-{}-beta.mat'.format(subject_num, model_type))
-    betas = np.array(betas['data'][above_threshold]).transpose()
-    latent_betas = betas[:LATENT_DIMENSION]
-    bias_beta = betas[LATENT_DIMENSION]
-
-    test_images = TEST_STIMULI['vaegan-sub-{:02d}-all'.format(subject_num)]
-
-    # The final betas are for the test images
-    ground_truth_voxels = betas[len(betas) - len(test_images):].transpose()
-
-    predicted_voxels = np.empty((len(test_images), n_voxels))
-    correlation = np.empty(n_voxels)
-    for index, test_image in enumerate(test_images):
-        celeba_file = os.path.join(args.celeba_dir, stimulus_to_celeba[test_image])
-        # (1 x 24)
-        latent_values = encode_img(celeba_file)
-        # (1 x n_voxels)
-        prediction = np.matmul(latent_values, latent_betas)
-        predicted_voxels[index] = prediction + bias_beta
-
-    predicted_voxels = predicted_voxels.transpose()
-    for i in range(len(correlation)):
-        correlation[i] = spearmanr(predicted_voxels[i], ground_truth_voxels[i]).correlation
-
-    '''
-    filtered_correlation = correlation.copy()
-    filtered_correlation[below_threshold] = 0
-    sio.savemat('threshold-{}-subject{}-{}.mat'.format(threshold, subject_num, model_type),
-                {'data': filtered_correlation})
-    average_correlation = np.sum(filtered_correlation) / num_above_threshold
-    print('Correlation: {}'.format(average_correlation))
-    '''
-
-    # Generate samples from a null distribution for significance testing
-    print('generated null hypothesis')
-    num_null_distribution_samples = 1000
-    null_hypothesis_average_correlations = np.empty(num_null_distribution_samples)
-    null_samples = np.empty((num_null_distribution_samples, len(test_images), n_voxels))
-    for i in range(num_null_distribution_samples):
-        null_hypothesis_correlation = np.empty(n_voxels)
-        null_hypothesis_ground_truth = ground_truth_voxels.copy()
-        np.random.shuffle(null_hypothesis_ground_truth)
-        for j in range(len(correlation)):
-            null_hypothesis_correlation[j] = spearmanr(predicted_voxels[j], null_hypothesis_ground_truth[j]).correlation
-        # Threshold the null hypothesis corerlation
-        null_hypothesis_average_correlations[i] = np.sum(null_hypothesis_correlation) / n_voxels
-
-    #correlation_vectors['subject{}-{}'.format(subject_num, model_type)] = correlation.copy()
-
-    sio.savemat('subject{}-{}.mat'.format(subject_num, model_type), {'data': correlation})
-    average_correlation = np.sum(correlation) / n_voxels
-    print('Correlation: {}'.format(average_correlation))
-
-    print('Number of correlation>null hypothesis correlation')
-    print(np.sum(average_correlation > null_hypothesis_average_correlations))
-    '''
     for threshold in THRESHOLDS:
         print('Threshold {}'.format(threshold))
-        filtered_correlation = correlation.copy()
-        below_threshold = localizer_map < threshold
-        num_above_threshold = len(localizer_map) - np.sum(below_threshold)
-        print('Number above threshold: {}'.format(num_above_threshold))
-        filtered_correlation[below_threshold] = 0
-        sio.savemat('threshold-{}-subject{}-{}.mat'.format(threshold, subject_num, model_type), {'data': filtered_correlation})
-        #correlation_vectors['filtered-subject{}-{}'.format(subject_num, model_type)] = filtered_correlation
-        average_correlation = np.sum(filtered_correlation) / num_above_threshold
-        print('Correlation: {}'.format(average_correlation))
-    '''
-#sio.savemat('correlation.mat', correlation_vectors)
+        above_threshold = localizer_map > threshold
+        n_voxels = np.sum(above_threshold)
+        print('Number above threshold: {}'.format(n_voxels))
 
+        print('Model {}'.format(model_type))
+        betas = sio.loadmat('betas/sub{:02d}_{}-beta.mat'.format(subject_num, model_type))
+        betas = np.array(betas['data'][above_threshold]).transpose()
+        latent_betas = betas[:LATENT_DIMENSION]
+        bias_beta = betas[LATENT_DIMENSION]
+
+        test_images = TEST_STIMULI['vaegan-sub-{:02d}-all'.format(subject_num)]
+
+        # The final betas are for the test images
+        ground_truth_voxels = betas[len(betas) - len(test_images):].transpose()
+
+        predicted_voxels = np.empty((len(test_images), n_voxels))
+        correlation = np.empty(n_voxels)
+        for index, test_image in enumerate(test_images):
+            celeba_file = os.path.join(args.celeba_dir, stimulus_to_celeba[test_image])
+            # (1 x 24)
+            latent_values = encode_img(celeba_file)
+            # (1 x n_voxels)
+            prediction = np.matmul(latent_values, latent_betas)
+            predicted_voxels[index] = prediction + bias_beta
+
+        predicted_voxels = predicted_voxels.transpose()
+        for i in range(len(correlation)):
+            correlation[i] = spearmanr(predicted_voxels[i], ground_truth_voxels[i]).correlation
+
+        sio.savemat('subject{}-{}.mat'.format(subject_num, model_type), {'data': correlation})
+        average_correlation = np.sum(correlation) / n_voxels
+        print('Correlation: {}'.format(average_correlation))
+
+        if not args.skip_p_value:
+            # Generate samples from a null distribution for significance testing
+            print('generating null hypothesis')
+            num_null_distribution_samples = 1000
+            null_hypothesis_average_correlations = np.empty(num_null_distribution_samples)
+            for i in range(num_null_distribution_samples):
+                null_hypothesis_correlation = np.empty(n_voxels)
+                null_hypothesis_ground_truth = ground_truth_voxels.copy()
+                # Shuffle the conditions within each voxels
+                np.apply_along_axis(np.random.shuffle, 1, null_hypothesis_ground_truth)
+                for j in range(len(correlation)):
+                    null_hypothesis_correlation[j] = spearmanr(predicted_voxels[j], null_hypothesis_ground_truth[j]).correlation
+                # Threshold the null hypothesis correlation
+                null_hypothesis_average_correlations[i] = np.sum(null_hypothesis_correlation) / n_voxels
+                # TODO save this array
+            print('Number of correlation>null hypothesis correlation')
+            print(np.sum(average_correlation > null_hypothesis_average_correlations))
