b9849d28b429703e7e4d5cacb78bbb3656f6df30
Paul Soulos <psoulos@gmail.com>
2022-02-15 15:02:32 -0500

    Convert whole brain correlation mat to niftis

--- convert_alignment_mat_to_nifti.m
+++ convert_alignment_mat_to_nifti.m
@@ -1,4 +1,5 @@
 assert(~(getenv('FIELDTRIP_DIR') == ""), 'You must first set the environment variable FIELDTRIP_DIR')
+assert(~(getenv('ALIGNMENT_DIR') == ""), 'You must first set the environment variable ALIGNMENT_DIR')
 assert(~(getenv('FUNCTIONALS_DIR') == ""), 'You must first set the environment variable FUNCTIONALS_DIR')
 assert(~(getenv('SUBJECTS_DIR') == ""), 'You must first set the environment variable SUBJECTS_DIR')
 assert(~(getenv('MODEL_NAME') == ""), 'You must first set the environment variable MODEL_NAME')
@@ -10,10 +11,10 @@ rois = {'FFA' 'OFA' 'STS'};
 for i = 1:length(subject_nums)
     subject_num = subject_nums{i}
     roi_dir = ([getenv('SUBJECTS_DIR') '/vaegan-sub-0' num2str(subject_num) '-all/roi/']);
-    correlations_dir = [getenv('FUNCTIONALS_DIR') '/vaegan-consolidated/unpackdata/vaegan-sub-0' num2str(subject_num) '-all/bold/correlations/'];
+    alignment_dir = getenv('ALIGNMENT_DIR');
     
     % Get the left and right betas so that we can use the metadata and
-    % overwrite the vol with the correlations
+    % overwrite the vol with the alignments
     left_hemi_beta_dir = [getenv('FUNCTIONALS_DIR') '/vaegan-consolidated/unpackdata/vaegan-sub-0' num2str(subject_num) '-all/bold/' getenv('MODEL_NAME') '.lh'];
     right_hemi_beta_dir = [getenv('FUNCTIONALS_DIR') '/vaegan-consolidated/unpackdata/vaegan-sub-0' num2str(subject_num) '-all/bold/' getenv('MODEL_NAME') '.rh'];
     left_hemi_betas = MRIread([left_hemi_beta_dir '/beta.nii.gz']);
@@ -24,39 +25,39 @@ for i = 1:length(subject_nums)
     
     for j = 1:length(rois)
         roi = rois{j}
-        correlation = load([correlations_dir getenv('MODEL_NAME') '.' roi '.correlations.mat']);
-        correlation = correlation.data;
+        alignment = load([alignment_dir '/subject' num2str(subject_num) '.' getenv('MODEL_NAME') '.' roi '.alignments.mat']);
+        alignment = alignment.data;
         left_roi = load([roi_dir 'l' roi '.surf.thresholded.both.mat']);
         left_roi = left_roi.threshold_roi;
         right_roi = load([roi_dir 'r' roi '.surf.thresholded.both.mat']);
         right_roi = right_roi.threshold_roi;
         
-        left_correlations = correlation(1:sum(left_roi));
-        right_correlations = correlation(sum(left_roi)+1:length(correlation));
+        left_alignments = alignment(1:sum(left_roi));
+        right_alignments = alignment(sum(left_roi)+1:length(alignment));
         
-        expanded_left_correlations = left_roi;
-        expanded_left_correlations(find(left_roi)) = left_correlations;
-        expanded_right_correlations = right_roi;
-        expanded_right_correlations(find(right_roi)) = right_correlations;
+        expanded_left_alignments = left_roi;
+        expanded_left_alignments(find(left_roi)) = left_alignments;
+        expanded_right_alignments = right_roi;
+        expanded_right_alignments(find(right_roi)) = right_alignments;
         
         
-        left_hemi_betas.vol = expanded_left_correlations;
-        left_hemi_betas.fspec = [correlations_dir getenv('MODEL_NAME') '.l' roi '.correlations.nii.gz'];
+        left_hemi_betas.vol = expanded_left_alignments;
+        left_hemi_betas.fspec = [alignment_dir '/subject' num2str(subject_num) '.' getenv('MODEL_NAME') '.l' roi '.alignments.nii.gz'];
         MRIwrite(left_hemi_betas, left_hemi_betas.fspec);
         
-        right_hemi_betas.vol = expanded_right_correlations;
-        right_hemi_betas.fspec = [correlations_dir getenv('MODEL_NAME') '.r' roi '.correlations.nii.gz'];
+        right_hemi_betas.vol = expanded_right_alignments;
+        right_hemi_betas.fspec = [alignment_dir '/subject' num2str(subject_num) '.' getenv('MODEL_NAME') '.r' roi '.alignments.nii.gz'];
         MRIwrite(right_hemi_betas, right_hemi_betas.fspec);
         
-        left_hemi_all_rois = left_hemi_all_rois + expanded_left_correlations;
-        right_hemi_all_rois = right_hemi_all_rois + expanded_right_correlations;
+        left_hemi_all_rois = left_hemi_all_rois + expanded_left_alignments;
+        right_hemi_all_rois = right_hemi_all_rois + expanded_right_alignments;
     end
     
     left_hemi_betas.vol = left_hemi_all_rois;
-    left_hemi_betas.fspec = [correlations_dir getenv('MODEL_NAME') '.left.correlations.nii.gz'];
+    left_hemi_betas.fspec = [alignment_dir '/subject' num2str(subject_num) '.' getenv('MODEL_NAME') '.left.alignments.nii.gz'];
     MRIwrite(left_hemi_betas, left_hemi_betas.fspec);
     
     right_hemi_betas.vol = right_hemi_all_rois;
-    right_hemi_betas.fspec = [correlations_dir getenv('MODEL_NAME') '.right.correlations.nii.gz'];
+    right_hemi_betas.fspec = [alignment_dir '/subject' num2str(subject_num) '.' getenv('MODEL_NAME') '.right.alignments.nii.gz'];
     MRIwrite(right_hemi_betas, right_hemi_betas.fspec);
 end
---
+++ convert_whole_brain_correlation_mat_to_nifti.m
@@ -0,0 +1,46 @@
+assert(~(getenv('FIELDTRIP_DIR') == ""), 'You must first set the environment variable FIELDTRIP_DIR')
+assert(~(getenv('FUNCTIONALS_DIR') == ""), 'You must first set the environment variable FUNCTIONALS_DIR')
+assert(~(getenv('SUBJECTS_DIR') == ""), 'You must first set the environment variable SUBJECTS_DIR')
+assert(~(getenv('MODEL_NAME') == ""), 'You must first set the environment variable MODEL_NAME')
+assert(~(getenv('SCORE_THRESHOLD') == ""), 'You must first set the environment variable SCORE_THRESHOLD')
+
+
+addpath([getenv('FIELDTRIP_DIR') '/external/freesurfer'])
+subject_nums = {1 2 3 4};
+hemis = {'l' 'r'};
+for i = 1:length(subject_nums)
+    subject_num = subject_nums{i}
+    roi_dir = ([getenv('SUBJECTS_DIR') '/vaegan-sub-0' num2str(subject_num) '-all/roi/']);
+    correlations_dir = [getenv('FUNCTIONALS_DIR') '/vaegan-consolidated/unpackdata/vaegan-sub-0' num2str(subject_num) '-all/bold/correlations/'];
+    
+    % Get the left and right betas so that we can use the metadata and
+    % overwrite the vol with the correlations
+    left_hemi_beta_dir = [getenv('FUNCTIONALS_DIR') '/vaegan-consolidated/unpackdata/vaegan-sub-0' num2str(subject_num) '-all/bold/' getenv('MODEL_NAME') '.lh'];
+    right_hemi_beta_dir = [getenv('FUNCTIONALS_DIR') '/vaegan-consolidated/unpackdata/vaegan-sub-0' num2str(subject_num) '-all/bold/' getenv('MODEL_NAME') '.rh'];
+    left_hemi_betas = MRIread([left_hemi_beta_dir '/beta.nii.gz']);
+    right_hemi_betas = MRIread([right_hemi_beta_dir '/beta.nii.gz']);
+
+    correlation = load([correlations_dir getenv('MODEL_NAME') '.whole.correlations.mat']);
+    correlation = correlation.data;
+
+    left_localizer = load([roi_dir 'whole_brain_score_' str2num(getenv('SCORE_THRESHOLD') '.lh.surf.thresholded.mat']);
+    left_localizer = left_localizer.left_score > 0
+    right_localizer = load([roi_dir 'whole_brain_score_' str2num(getenv('SCORE_THRESHOLD') '.rh.surf.thresholded.mat']);
+    right_localizer = right_localizer.right_score > 0
+
+    left_correlations = correlation(1:sum(left_localizer));
+    right_correlations = correlation(sum(left_localizer)+1:length(correlation));
+
+    expanded_left_correlations = left_roi;
+    expanded_left_correlations(find(left_roi)) = left_correlations;
+    expanded_right_correlations = right_roi;
+    expanded_right_correlations(find(right_roi)) = right_correlations;
+
+    left_hemi_betas.vol = expanded_left_correlations;
+    left_hemi_betas.fspec = [correlations_dir getenv('MODEL_NAME') 'score.lh.correlations.nii.gz'];
+    MRIwrite(left_hemi_betas, left_hemi_betas.fspec);
+
+    right_hemi_betas.vol = expanded_right_correlations;
+    right_hemi_betas.fspec = [correlations_dir getenv('MODEL_NAME') 'score.rh.correlations.nii.gz'];
+    MRIwrite(right_hemi_betas, right_hemi_betas.fspec);
+end
